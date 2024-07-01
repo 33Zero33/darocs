d97942865ae6801099d3e204bd956a1c560a4327
33Zero33 <cdaroca37@gmail.com>
2024-07-01 22:58:18 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const section = document.querySelectorAll('section');
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        if (top > offset && top < offset + height){
+            sec.classList.add('start-animation')
+        }
+
+        else{
+            sec.classList.remove('start-animation')
+        }
+     })
+}
+
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
